95afba7e13a29c89f079ee7737ca188cdf1d0c1a
daurenKa <dauren.kass@gmail.com>
2021-05-02 16:13:08 +0600

    remove: https

--- src/Services/Catalog/Catalog.API/Startup.cs
+++ src/Services/Catalog/Catalog.API/Startup.cs
@@ -49,8 +49,6 @@ namespace Catalog.API
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Catalog.API v1"));
             }
 
-            app.UseHttpsRedirection();
-
             app.UseRouting();
 
             app.UseAuthorization();
