2b4da324cafd35c8a4443de4472bfc7620ebc964
daurenKa <dauren.kass@gmail.com>
2021-05-03 09:31:22 +0600

    add: basket repo and api

---
+++ src/Services/Basket/Basket.API/Entities/ShoppingCartItem.cs
@@ -0,0 +1,12 @@
+namespace Basket.API.Entities
+{
+    public class ShoppingCartItem
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+        public string Color { get; set; }
+    }
+}
--- src/Services/Basket/Basket.API/Startup.cs
+++ src/Services/Basket/Basket.API/Startup.cs
@@ -1,15 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Basket.API.Repos;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 
 namespace Basket.API
@@ -30,6 +24,7 @@ namespace Basket.API
             {
                 options.Configuration = Configuration.GetValue<string>("CacheSettings:ConnectionString");
             });
+            services.AddScoped<IBasketRepo, BasketRepo>();
             
             services.AddControllers();
             services.AddSwaggerGen(c =>
