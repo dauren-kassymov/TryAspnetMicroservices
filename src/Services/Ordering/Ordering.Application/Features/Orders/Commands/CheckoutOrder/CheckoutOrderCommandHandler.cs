7c03f7b46ffe0a14ac9e73ea35d02a292a7499a8
DaurenK <dauren.kass@gmail.com>
2021-05-08 17:17:11 +0600

    WIP

---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Commands/CheckoutOrder/CheckoutOrderCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using MediatR;
+using Ordering.Application.Contracts.Infrastructure;
+using Ordering.Application.Contracts.Persistence;
+
+namespace Ordering.Application.Features.Orders.Commands.CheckoutOrder
+{
+    public class CheckoutOrderCommandHandler : IRequestHandler<CheckoutOrderCommand, int>
+    {
+        private readonly IOrderRepo _orderRepo;
+        private readonly IMapper _mapper;
+        private readonly IEmailService _emailService;
+
+        public CheckoutOrderCommandHandler(
+            IMapper mapper, 
+            IOrderRepo orderRepo, 
+            IEmailService emailService)
+        {
+            _mapper = mapper;
+            _orderRepo = orderRepo;
+            _emailService = emailService;
+        }
+
+        public Task<int> Handle(CheckoutOrderCommand request, CancellationToken cancellationToken)
+        {
+            
+        }
+    }
+}
