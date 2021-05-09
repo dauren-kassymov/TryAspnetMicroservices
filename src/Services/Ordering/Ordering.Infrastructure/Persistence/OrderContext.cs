6f340e0e1c17f3dc9175fbbd4a7bba1116fdbc47
DaurenK <dauren.kass@gmail.com>
2021-05-09 13:30:48 +0600

    wip

---
+++ src/Services/Ordering/Ordering.Infrastructure/Persistence/OrderContext.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Ordering.Domain.Common;
+using Ordering.Domain.Entities;
+
+namespace Ordering.Infrastructure.Persistence
+{
+    public class OrderContext : DbContext
+    {
+        public OrderContext(DbContextOptions<OrderContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Order> Orders { get; set; }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            foreach (var entry in ChangeTracker.Entries<EntityBase>())
+            {
+                switch (entry.State)
+                {
+                    case EntityState.Added:
+                        entry.Entity.CreatedDate = DateTime.UtcNow;
+                        entry.Entity.CreatedBy = "HARDCODED_USERNAME";
+                        break;
+                    case EntityState.Modified:
+                        entry.Entity.LastModifiedDate = DateTime.UtcNow;
+                        entry.Entity.LastModifiedBy = "HARDCODED_USERNAME";
+                        break;
+                }
+            }
+
+            return base.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
---
+++ src/Services/Ordering/Ordering.Infrastructure/Persistence/OrderContextSeed.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Ordering.Domain.Entities;
+
+namespace Ordering.Infrastructure.Persistence
+{
+    public class OrderContextSeed
+    {
+        public static async Task SeedAsync(OrderContext context, ILogger<OrderContextSeed> logger)
+        {
+            if (!context.Orders.Any())
+            {
+                context.Orders.AddRange(GetOrders());
+            }
+        }
+
+        private static IEnumerable<Order> GetOrders()
+        {
+            return Enumerable.Range(1, 1)
+                .Select(x => new Order
+                {
+                    UserName = $"user-{x}",
+                    FirstName = $"fname-{x}",
+                    LastName = $"lname-{x}",
+                    EmailAddress = $"email-{x}",
+                    TotalPrice = x * 100
+                });
+        }
+    }
+}
