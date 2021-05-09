0bf0531c073f890c20b95960062f5f03210f3099
DaurenK <dauren.kass@gmail.com>
2021-05-09 10:05:54 +0600

    feat(Behaviours): add Validation\UnhandledException behaviours

---
+++ src/Services/Ordering/Ordering.Application/Behaviours/UnhandledExceptionBehaviour.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace Ordering.Application.Behaviours
+{
+    public class UnhandledExceptionBehaviour<TReq, TRes> : IPipelineBehavior<TReq, TRes>
+    {
+        private readonly ILogger<TRes> _logger;
+
+        public UnhandledExceptionBehaviour(ILogger<TRes> logger)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        public async Task<TRes> Handle(TReq request, CancellationToken cancellationToken, RequestHandlerDelegate<TRes> next)
+        {
+            try
+            {
+                return await next();
+            }
+            catch (Exception ex)
+            {
+                var requestName = typeof(TReq).Name;
+                _logger.LogError(ex, "Application Request: Unhandled Exception for Request {Name} {@Request}", requestName, request); //TODO:
+                throw;
+            }
+        }
+    }
+}
--- src/Services/Ordering/Ordering.Application/Behaviours/ValidationBehaviour.cs
+++ src/Services/Ordering/Ordering.Application/Behaviours/ValidationBehaviour.cs
@@ -1,14 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using FluentValidation;
 using MediatR;
+using ValidationException = Ordering.Application.Exceptions.ValidationException;
 
 namespace Ordering.Application.Behaviours
 {
     public class ValidationBehaviour<TReq, TRes>: IPipelineBehavior<TReq, TRes>
     {
-        public Task<TRes> Handle(TReq request, CancellationToken cancellationToken, RequestHandlerDelegate<TRes> next)
+        private readonly IEnumerable<IValidator<TReq>> _validators;
+
+        public ValidationBehaviour(IEnumerable<IValidator<TReq>> validators)
         {
-            throw new System.NotImplementedException();
+            _validators = validators ?? throw new ArgumentNullException(nameof(validators));
+        }
+
+        public async Task<TRes> Handle(TReq request, CancellationToken cancellationToken, RequestHandlerDelegate<TRes> next)
+        {
+            if (_validators.Any())
+            {
+                var context = new ValidationContext<TReq>(request);
+                var validationResults = await Task.WhenAll(_validators
+                    .Select(x => x.ValidateAsync(context, cancellationToken))
+                );
+                var failures = validationResults
+                    .SelectMany(x => x.Errors)
+                    .Where(x => x != null)
+                    .ToList();
+                if (failures.Any())
+                {
+                    throw new ValidationException(failures);
+                }
+            }
+
+            return await next();
         }
     }
 }
