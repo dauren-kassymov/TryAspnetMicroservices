e47765d8d4719cf9274a044c8d476b51937d1a82
daurenKa <dauren.kass@gmail.com>
2021-05-09 17:05:22 +0600

    feat(EmailService): send email using SendGrid

---
+++ src/Services/Ordering/Ordering.Infrastructure/Email/EmailService.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Ordering.Application.Contracts.Infrastructure;
+using Ordering.Application.Models;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+
+namespace Ordering.Infrastructure.Email
+{
+    public class EmailService : IEmailService
+    {
+        private readonly EmailSettings _emailSettings;
+        private readonly Logger<EmailService> _logger;
+
+        public EmailService(IOptions<EmailSettings> emailSettings, Logger<EmailService> logger)
+        {
+            _emailSettings = emailSettings.Value ?? throw new ArgumentNullException(nameof(emailSettings));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        public async Task<bool> SendEmail(Application.Models.Email email)
+        {
+            var client = new SendGridClient(_emailSettings.ApiKey);
+
+            var to = new EmailAddress(email.To);
+            var subject = email.Subject;
+            var body = email.Body;
+
+            var from = new EmailAddress(_emailSettings.FromAddress, _emailSettings.FromName);
+
+            var message = MailHelper.CreateSingleEmail(@from, to, subject, body, body);
+            _logger.LogInformation("Email sending...");
+            var response = await client.SendEmailAsync(message);
+            if (response.StatusCode == HttpStatusCode.Accepted || response.StatusCode == HttpStatusCode.OK)
+                return true;
+
+            _logger.LogError("Email sending failed");
+            return false;
+        }
+    }
+}
