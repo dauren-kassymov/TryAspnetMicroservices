26630fb6ba13b43ad736fead1c21e3bc36bc7321
DaurenK <dauren.kass@gmail.com>
2021-05-10 12:25:01 +0600

    feat: seed Db

--- src/Services/Ordering/Ordering.API/Program.cs
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -1,11 +1,10 @@
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
+using Ordering.API.Extensions;
+using Ordering.Infrastructure.Persistence;
 
 namespace Ordering.API
 {
@@ -13,7 +12,10 @@ namespace Ordering.API
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            CreateHostBuilder(args)
+                .Build()
+                .MigrateDb<OrderContext>(Seed)
+                .Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
@@ -22,5 +24,11 @@ namespace Ordering.API
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+
+        private static void Seed(OrderContext context, IServiceProvider services)
+        {
+            var logger = services.GetService<ILogger<OrderContextSeed>>();
+            OrderContextSeed.SeedAsync(context, logger).Wait();
+        }
     }
 }
