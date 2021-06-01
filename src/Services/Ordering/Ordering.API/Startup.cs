cf866393e33bc9841cbe39b5329fde5d853690f5
daurenKa <dauren.kass@gmail.com>
2021-06-01 15:05:50 +0600

    feat: BasketCheckoutConsumer

--- src/Services/Ordering/Ordering.API/Startup.cs
+++ src/Services/Ordering/Ordering.API/Startup.cs
@@ -50,6 +50,10 @@ namespace Ordering.API
             });
             services.AddMassTransitHostedService();
             
+            //General config
+            services.AddAutoMapper(typeof(Startup));
+            services.AddScoped<BasketCheckoutConsumer>();
+
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
